35de78acb7a10688e3228dcb6557d69978f3f30b
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2022-08-21 18:53:07 +0900

    Updates

---
+++ component---src-pages-page-2-js-443da8572264ebd4f51e.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_hello_world=self.webpackChunkgatsby_starter_hello_world||[]).push([[617],{1800:function(e,t,r){r.r(t),r.d(t,{default:function(){return a}});var l=r(7294),n=r(1597);function a(){return l.createElement(l.Fragment,null,l.createElement("div",null,"Page2"),l.createElement(n.rU,{to:"/"},"ホームに戻る"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-443da8572264ebd4f51e.js.map
